3accefa656ee76987addfe643a5ff1119b77cf41
khanhthanhdev <thanhkt27507@gmail.com>
2023-07-08 10:49:51 +0700

    interface setup

---
+++ types.ts
@@ -0,0 +1,13 @@
+
+
+export interface Billboard {
+    id: string;
+    name: string;
+    imageUrl: string;
+}
+
+export interface Category {
+    id: string;
+    name: string;
+    billboard: Billboard;
+}
